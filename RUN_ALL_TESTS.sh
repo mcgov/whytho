e2d867b8816028b588a9cb6b1abd519d93b69a77
mcgov <matthew@mcgov.dev>
2022-08-25 14:09:32 -0700

    update perms and comments

--- RUN_ALL_TESTS.sh
+++ RUN_ALL_TESTS.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 test -d results || mkdir results
 touch results/all_results.log
-echo "NOTE: This will take a few minutes..."
+echo "NOTE: This will take a while. Now might be a good time to get some coffee?"
 ls run_*.sh | xargs -t -I % sh -c "./%; exit 0;" &> results/all_results.log
 echo "All done :) results are in results/all_results.log"
 echo "$(hostname) ------------" >> results/dmesg.log

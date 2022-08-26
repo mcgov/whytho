899706128d852e261cf89957e804a829d8ebcd3f
mcgov <matthew@mcgov.dev>
2022-08-26 16:30:26 -0700

    turn off opt

--- run_mnuma.sh
+++ run_mnuma.sh
@@ -2,7 +2,7 @@ source ./common.sh
 
 install_dependencies lscpu gcc
 
-gcc mnuma.c -O1 -o mnuma --std=gnu11
+gcc mnuma.c -O0 -o mnuma --std=gnu11
 ARGS=`lscpu -C -B | tail -4 | awk ' { print $2 } ' | tr "\n" " " `
 ./mnuma $ARGS
 rm ./mnuma

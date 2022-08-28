7bfbbe23811f5dfb4bcb1e09f7f9023708448b91
mcgov <matthew@mcgov.dev>
2022-08-28 11:59:20 -0700

    rename to memnuma

--- run_memnuma.sh
+++ run_memnuma.sh
@@ -2,8 +2,8 @@ source ./common.sh
 
 install_dependencies lscpu gcc lsmem
 
-gcc mnuma.c -O1 -o mnuma --std=gnu11
+gcc memnuma.c -O1 -o memnuma --std=gnu11
 ARGS=`lscpu -C -B | tail -4 | awk ' { print $2 } ' | tr "\n" " " `
-sudo ./mnuma $ARGS
-rm ./mnuma
+sudo ./memnuma $ARGS
+rm ./memnuma
 lsmem --output-all

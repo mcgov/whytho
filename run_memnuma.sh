b5c34e0f776534b86c8adb7e45bc6ae9319f71d5
mcgov <matthew@mcgov.dev>
2022-08-28 12:36:39 -0700

    turn off loop opt

--- run_memnuma.sh
+++ run_memnuma.sh
@@ -2,7 +2,7 @@ source ./common.sh
 
 install_dependencies lscpu gcc lsmem
 
-gcc memnuma.c -O1 -o memnuma --std=gnu11
+gcc memnuma.c -O1 -o memnuma --std=gnu11 -fno-aggressive-loop-optimizations
 ARGS=`lscpu -C -B | tail -4 | awk ' { print $2 } ' | tr "\n" " " `
 sudo ./memnuma $ARGS
 rm ./memnuma

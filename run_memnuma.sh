06d54cfb9b6e6e2b1b1bf9f5a09e39fc4429a86c
mcgov <matthew@mcgov.dev>
2022-08-28 20:34:02 -0700

    fix args for memnuma

--- run_memnuma.sh
+++ run_memnuma.sh
@@ -3,6 +3,9 @@ source ./common.sh
 install_dependencies lscpu gcc lsmem
 
 gcc memnuma.c -O1 -o memnuma --std=gnu11 -fno-aggressive-loop-optimizations --debug
-ARGS=`lscpu -C -B | tail -4 | awk ' { print $2 } ' | tr "\n" " " `
-lscpu -C -B && sudo ./memnuma $ARGS && rm ./memnuma
+
+# some versions of lspcu don't support the -B flag
+ARGS=`getconf -a | grep CACHE_SIZE  | grep -v LEVEL_4 | sort  | awk ' { print $2 } ' | tr "\n" " " `
+echo $ARGS
+sudo ./memnuma $ARGS && rm ./memnuma
 lsmem --output-all

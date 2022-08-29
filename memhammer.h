16c874d3f7336efb6e075cca7a6323c8bd1f9609
mcgov <matthew@mcgov.dev>
2022-08-29 14:18:18 -0700

    add max/min and account for extra work time in timer

--- memhammer.h
+++ memhammer.h
@@ -11,14 +11,16 @@
 #define ROL8(value) ((((size_t)value) << 8) | (((size_t)value) >> (sizeof(size_t) * (sizeof(size_t) - 1))))
 #define ITERATIONS 0x200
 
-
 size_t hammer_memory(size_t *allocation)
 {
     size_t timer_samples = 0;
     size_t value = 0xDEADBEEF;
     time_t epoch_start = time(0);
     size_t array_elements = GIBIBYTE / sizeof(size_t);
-    uint64_t loop_start, loop_end, iter_start;
+    uint64_t loop_start, loop_end, iter_start, max = 0, min = UINT64_MAX, loop_work_time = 0;
+    size_t element_accesses =  (5 * array_elements);
+
+
     uint64_t iteration_timers[ITERATIONS] = {};
     loop_start = __rdtsc();
     for (int repeat = 0; repeat < ITERATIONS; repeat++)
@@ -34,7 +36,7 @@ size_t hammer_memory(size_t *allocation)
         for (size_t i = 0; i < array_elements; i++)
         {
             // smash some bits in sequence
-            allocation[(i^iter_start)%array_elements] = allocation[i] ^ value * 0xB4DD1EB00;
+            allocation[(i ^ iter_start) % array_elements] = allocation[i] ^ value * 0xB4DD1EB00;
         }
         // get weird
         for (size_t i = 0; i < array_elements; i++)
@@ -53,17 +55,26 @@ size_t hammer_memory(size_t *allocation)
             allocation[(i * epoch_start) % (array_elements)] = allocation[i] ^ 0xB4ff1ed * value;
         }
         iteration_timers[repeat] = __rdtsc() - iter_start;
+        if (iteration_timers[repeat] > max)
+            max = iteration_timers[repeat];
+        if (iteration_timers[repeat] < min)
+            min = iteration_timers[repeat];
     }
     loop_end = __rdtsc();
-
-    for (int i = 0; i < ITERATIONS; i++){
-        printf("Access time for iteration %d: %ld (total ticks) %ld (average per element)\n", i, iteration_timers[i], iteration_timers[i]/(5*array_elements));
+    for (int i = 0; i < ITERATIONS; i++)
+    {
+        printf("Access time for iteration %d: %ld (total ticks) %ld (average per element)\n",
+               i, iteration_timers[i], iteration_timers[i] / element_accesses);
+        loop_work_time += iteration_timers[i];
     }
+
     printf(
-        "Iterated in %ld (total ticks), average ticks/element in %ld ticks\n",
-         loop_end-loop_start,
-         (loop_end-loop_start)/(ITERATIONS*5*array_elements)
-        );
+        "Iterated in %ld (total) %ld (work), average ticks/element in %ld ticks, max: %ld min: %ld\n",
+        loop_end - loop_start,
+        loop_work_time,
+        loop_work_time / (ITERATIONS * element_accesses),
+        max / element_accesses,
+        min / element_accesses);
 
     return allocation[allocation[epoch_start * loop_start % (array_elements)] % (array_elements)];
 }

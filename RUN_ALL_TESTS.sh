19aafa5edd076f3872acb1cb9d11d63c73c319c8
mcgov <matthew@mcgov.dev>
2022-08-29 12:22:36 -0700

    update script output to include seperate dmesg run

--- RUN_ALL_TESTS.sh
+++ RUN_ALL_TESTS.sh
@@ -3,6 +3,7 @@ test -d results || mkdir results
 touch results/all_results.log
 echo "NOTE: This will take a while. Now might be a good time to get some coffee?"
 ls run_*.sh | xargs -t -I % sh -c "./%; exit 0;" &> results/all_results.log
-echo "All done :) results are in results/all_results.log"
+echo "All done :) running dmesg..."
 echo "$(hostname) ------------" >> results/dmesg.log
 sudo dmesg >> results/dmesg.log
+echo "all done! Results are in results/*.log"

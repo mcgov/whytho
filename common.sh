483c76d5005ca6b24fe657e765ca11a2446061c3
mcgov <matthew@mcgov.dev>
2022-09-07 15:31:02 -0700

    remove ;

--- common.sh
+++ common.sh
@@ -7,7 +7,7 @@ if [ "$WHYTHO_DEBUG" == "1" ]; then
     echo "Running with debug options (xargs should fail on nonzero exit code)"
     export WHYTHO_DEBUG_ACTIONS="|| exit 255"
 else
-    export WHYTHO_DEBUG_ACTIONS="exit 0;"
+    export WHYTHO_DEBUG_ACTIONS="exit 0"
 fi
 
 detect_package_manager() {

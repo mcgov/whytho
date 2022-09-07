890a3237befce192b03d5643c5248be18827ac43
mcgov <matthew@mcgov.dev>
2022-09-07 13:45:28 -0700

    update info runner

--- RUN_INFO_SCRIPTS.sh
+++ RUN_INFO_SCRIPTS.sh
@@ -7,15 +7,10 @@ else
     INFO_DEST="results/info_results-$1.log"
 fi
 
-if [ "$2" == "--debug" ]; then
-    ALLOW_ERRORS=""
-else
-    ALLOW_ERRORS="exit 0;"
-fi
 
 touch $INFO_DEST
 echo "Gathering info. NOTE: This will take a short while, just gathering info..."
-ls run_*.sh | xargs -t -I % sh -c "./%; $ALLOW_ERRORS" &> $INFO_DEST
+ls run_*.sh | xargs -n 1 -t -I % sh -c "./%; $WHYTHO_DEBUG_ACTIONS" &> $INFO_DEST
 if ! [ -e results/dmesg.log ];
 then
     echo "$(hostname) ------------" >> results/dmesg.log

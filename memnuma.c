28ca9fded655b1d268e7b588f57e827eb978da6b
mcgov <matthew@mcgov.dev>
2022-09-08 10:29:27 -0700

    cleanup memnuma

--- memnuma.c
+++ memnuma.c
@@ -15,7 +15,7 @@
 #include <errno.h>
 
 /*
-    mnuma.c
+    memnuma.c
     let's poke caches and look at the physical page info
 
     author: Matthew G. McGovern matthew@mcgov.dev
@@ -23,9 +23,6 @@
 */
 
 #define CACHE_LEVELS 3
-
-#pragma GCC push_options
-#pragma GCC optimize("O0")
 
 size_t next_highest_pow2(size_t n)
 {
@@ -55,17 +52,19 @@ struct cache_list *create_cache_list(size_t cache_size, size_t per_cache, size_t
         cache_size = pow2_check;
     }
 
-    struct cache_list *first = (struct cache_list *)aligned_alloc(cache_size, span_caches*cache_size);
+    struct cache_list *first = (struct cache_list *)aligned_alloc(cache_size, span_caches * cache_size);
     struct cache_list *next = first;
     memset(first, '\0', cache_size);
     while (--elements > 0)
-    { // classic
+    {
         next->data_sz = element_size;
         next->next = (struct cache_list *)(((uint8_t *)next) + element_size);
         next = next->next;
     }
     return first;
 }
+#pragma GCC push_options
+#pragma GCC optimize("O0")
 
 uint64_t time_accesses(struct cache_list *allocation, size_t element_count)
 {
@@ -82,17 +81,15 @@ uint64_t time_accesses(struct cache_list *allocation, size_t element_count)
     end = __rdtsc();
     printf("List iterated in %lu clock ticks\n", (end - start) / element_count);
 }
+#pragma GCC pop_options
 
-void train_and_access(size_t *cache_sizes, size_t numer_of_caches, size_t per_cache, size_t span_caches, struct cache_list **allocation_out)
+void run_cache_access_test(size_t *cache_sizes, size_t numer_of_caches, size_t per_cache, size_t span_caches, struct cache_list **allocation_out)
 {
-    size_t cache_size, allocation_size, start_address, end_address;
-    uint64_t start, end;
+    size_t cache_size, allocation_size;
     struct cache_list *allocation;
-    time_t time_ = time(0);
 
     for (int cache_level = 0; cache_level < numer_of_caches; cache_level++)
     {
-
         // get cache size
         cache_size = cache_sizes[cache_level];
         // determine size of allocation
@@ -110,7 +107,6 @@ void train_and_access(size_t *cache_sizes, size_t numer_of_caches, size_t per_ca
         allocation_out[cache_level] = allocation; // save off the allocation
     }
 }
-#pragma GCC pop_options
 
 int main(int argc, char **argv)
 {
@@ -133,11 +129,15 @@ int main(int argc, char **argv)
     printf("            --- Welcome to memnuma!  <('-'<) --- ");
     printf("Found cache sizes l1d %lx l1i %lx l2 %lx l3 %lx \n", l1d, l1i, l2, l3);
 
-    // run the training and accessing (not guaranteed to work as expected yet)
+    // set up some cache information and test constants
     size_t cache_sizes[] = {l1d, l2, l3};
     struct cache_list *allocations[CACHE_LEVELS] = {};
     size_t elements_per_cache = 0x100, cache_count = 0x100;
-    train_and_access(cache_sizes, CACHE_LEVELS, elements_per_cache, cache_count, allocations);
+
+    // run the cache access test:
+    // list iteration of multiple sub-cache sized objects
+    // across multiple cache sized allocations
+    run_cache_access_test(cache_sizes, CACHE_LEVELS, elements_per_cache, cache_count, allocations);
 
     // get pagemap filename
     char filename[0x100];
@@ -152,7 +152,7 @@ int main(int argc, char **argv)
     }
     printf("Opened %s\n", filename);
 
-    // get the physical address of the pages we used
+    // get the physical address of the pages we used (not guaranteed to be accurate on VMs)
     for (int cache_level = 0; cache_level < CACHE_LEVELS; cache_level++)
     {
 
@@ -162,7 +162,7 @@ int main(int argc, char **argv)
         size_t addresses[] = {start_address, end_address};
         int numa_node = -1;
         long result = 0;
-        for (size_t current_address = start_address; current_address < end_address; current_address+=(cache_sizes[cache_level]/elements_per_cache))
+        for (size_t current_address = start_address; current_address < end_address; current_address += (cache_sizes[cache_level] / elements_per_cache))
         {
             uint64_t data;
             off_t index = (off_t)((current_address) / 0x1000) * sizeof(data);
@@ -172,24 +172,26 @@ int main(int argc, char **argv)
                 goto CLOSE_FD;
             }
             printf("Cache L%d allocation was at:"
-                    " 0x%016lx (virtual)  0x%016lx (physical) on NUMA ",
-                    cache_level,
-                    (uint64_t)current_address,
-                    (data & 0x7fffffffffffff) * 0x1000);
+                   " 0x%016lx (virtual)  0x%016lx (physical) on NUMA ",
+                   cache_level,
+                   (uint64_t)current_address,
+                   (data & 0x7fffffffffffff) * 0x1000);
 
+            // get the numa assignment info of the memory region
+            result = get_mempolicy(&numa_node, NULL, 0, (void *)current_address, MPOL_F_NODE | MPOL_F_ADDR);
 
-            result = get_mempolicy(&numa_node, NULL, 0, (void*)current_address, MPOL_F_NODE | MPOL_F_ADDR);
-
-            if (result == 0){
+            if (result == 0)
+            {
                 printf("0x%04x (found)\n", numa_node);
-            } else {
-                printf("0x____ (%s)\n", strerror(errno) );
             }
-
-
+            else
+            {
+                printf("0x____ (%s)\n", strerror(errno));
+            }
         }
     }
 
+// cleanup and close
 CLOSE_FD:
     close(fd);
 FREE_ALLOCATIONS:

4866f15c039a654490b6cbe4222a851e0ea69c77
mcgov <matthew@mcgov.dev>
2022-09-08 09:50:52 -0700

    lscpu mod to output all extended columns that are available

--- run_lscpu.sh
+++ run_lscpu.sh
@@ -3,4 +3,4 @@
 source ./common.sh
 install_dependencies lscpu
 sudo lscpu
-sudo lscpu -e=CPU,BOGOMIPS,CORE,SOCKET,CLUSTER,NODE,BOOK,DRAWER,CACHE,POLARIZATION,ADDRESS,CONFIGURED,ONLINE,MHZ,MAXMHZ,MINMHZ
+sudo lscpu -a -e

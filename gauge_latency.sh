f5cff5865007349d096e05e3478303f78c1c0095
mcgov <matthew@mcgov.dev>
2022-09-14 15:12:08 -0700

    make memory total block smaller:

--- gauge_latency.sh
+++ gauge_latency.sh
@@ -2,5 +2,5 @@ source ./common.sh
 
 install_sysbench
 
-sysbench memory  --memory-access-mode=rnd --memory-total-size=28G --memory-block-size=512M run
+sysbench memory  --memory-access-mode=rnd --memory-total-size=12G --memory-block-size=512M run
 
